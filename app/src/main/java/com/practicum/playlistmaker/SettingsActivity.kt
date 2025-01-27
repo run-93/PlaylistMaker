81fe38bb6b9958e57cee0c464acb4651d3e0d2d7
RUN_93 <lenskiy19@gmail.com>
2025-01-27 19:07:41 +0300

    Домашяя работа, спринт № 8.

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.practicum.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        val buttonBack = findViewById<ImageView>(R.id.buttonBack)
+
+        buttonBack.setOnClickListener{
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+
+    }
+}
