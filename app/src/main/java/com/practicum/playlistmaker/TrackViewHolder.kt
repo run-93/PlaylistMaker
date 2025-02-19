14191a676f04d19f1ec84fe7deb57c4668b9d37a
run-93 <lenskiy19@gmail.com>
2025-02-19 17:46:46 +0300

    в работе

---
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,17 @@
+package com.practicum.playlistmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    /// определяем переменные и привязываем их к view
+
+    fun bind(item: Track) {
+        // связываем переменные с значениями из полей view
+
+    }
+
+
+        /// переопределяем значения
+}
